644265fca5014f7bee5b86deb9685f020e503ace
Muhammad Rifqi Adli Gumay <88142116+asta-kunn@users.noreply.github.com>
2023-08-10 21:47:02 +0700

    add dist

---
+++ dist/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { config } = require('dotenv');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+config();
+const app = express();
+const port = process.env.PORT || 8080;
+app.use(bodyParser.json());
+app.use(cors());
+mongoose
+    .connect(process.env.DB_URL)
+    .then(() => {
+    console.log("Connected to the database!");
+})
+    .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+});
+app.get("/", (req, res) => {
+    res.send("Welcome to AIC Compfest Back End!");
+});
+// import routes
+require("./routes/users")(app);
+require("./routes/posts")(app);
+require("./routes/comments")(app);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
+//# sourceMappingURL=index.js.map
---
+++ dist/routes/comments.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const auth_1 = __importDefault(require("../middleware/auth"));
+module.exports = (app) => {
+    const comments = require("../controllers/comments");
+    var router = require("express").Router();
+    router.post("/", auth_1.default, comments.createComment);
+    router.delete("/:commentId", auth_1.default, comments.deleteComment);
+    app.use("/api/comment", router);
+};
+//# sourceMappingURL=comments.js.map
